00ff9415912f83d232d316f74cb1c04759a2443c
Alberto <20241093@uthh.edu.mx>
2026-03-19 19:33:25 -0600

    si

--- src/controllers/controller.novedades.js
+++ src/controllers/controller.novedades.js
@@ -1,63 +1,61 @@
 import * as novedadesModel from '../models/models.novedades.js'
 
-// GET /api/
+// GET /api/novedades
 export const getAllNovedades = async (req, res) => {
-  try {
-    const novedade = await novedadesModel.getAllNovedades()
-    res.status(200).json(novedade)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
+    try {
+        const novedades = await novedadesModel.getAllNovedades()
+        res.status(200).json(novedades)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 }
 
-// GET /api/
+// GET /api/novedades/:id
 export const getNovedadesById = async (req, res) => {
-  try {
-    const novedad = await novedadesModel.getNovedadesById(req.params.id)
-
-    if (!novedad) {
-      return res.status(404).json({
-        message: 'novedad no encontrado'
-      })
+    try {
+        const novedad = await novedadesModel.getNovedadesById(req.params.id)
+        if (!novedad) {
+            return res.status(404).json({ message: 'Novedad no encontrada' })
+        }
+        res.status(200).json(novedad) 
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
-
-    res.status(200).json(marca)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
 }
 
-// POST /api/
+// POST /api/novedades
 export const createNovedad = async (req, res) => {
-  try {
-    const { tituloNovedad, encabezado, informacion } = req.body;
-    
-    // Multer pone la URL de Cloudinary en req.file.path
-    let Imagen = ''; 
-    let nombreImagen = 'Sin nombre';
+    try {
+        const { tituloNovedad, encabezado, informacion } = req.body;
+        
+       
+        let imagen = ''; 
+        let nombreImagen = 'Sin nombre';
 
-    if (req.file) {
-      Imagen = req.file.path; // URL de Cloudinary
-      nombreImagen = req.file.filename; // ID público en Cloudinary
+        if (req.file) {
+            imagen = req.file.path; 
+            nombreImagen = req.file.filename; 
+        }
+
+        if (!tituloNovedad || !encabezado || !informacion) {
+            return res.status(400).json({ message: 'Campos obligatorios faltantes' });
+        }
+
+        const nuevoNovedad = await novedadesModel.createNovedad({
+            tituloNovedad,
+            encabezado,
+            informacion,
+            nombreImagen,
+            imagen, 
+            fecha: new Date() 
+        });
+
+        res.status(201).json(nuevoNovedad);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-
-    if (!tituloNovedad || !encabezado || !informacion) {
-      return res.status(400).json({ message: 'Campos obligatorios faltantes' });
-    }
-
-    const nuevoNovedad = await novedadesModel.createNovedad({
-      tituloNovedad,
-      encabezado,
-      informacion,
-      nombreImagen,
-      Imagen
-    });
-
-    res.status(201).json(nuevoNovedad);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 };
+
 
 export const updateNovedad = async (req, res) => {
     try {
